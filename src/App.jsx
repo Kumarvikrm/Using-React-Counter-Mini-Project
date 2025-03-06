59c1f5fa266514269407e3967cca3bf53a664d0e
Kumarvikrm <kumarvikram68715@gmail.com>
2025-03-06 18:24:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EvenOddApp from './Components/EvenOddApp'
+
+const App = () => {
+  return (
+    <div>
+      <EvenOddApp/>
+    </div>
+  )
+}
+
+export default App
